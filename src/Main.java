01c674cd000db5e597af95ee5261a87da36953f5
imba1401 <imba_olga@mail.ru>
2024-09-16 17:04:00 +0300

    Day 1 task 1-4

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+//        int counter = 0;                //1 задание
+//        while (counter < 10){
+//            System.out.print ("JAVA ");
+//            counter++;}
+//
+//
+//       for (int i = 1; i< 11; i++){        //2 задание
+//            System.out.print("JAVA ");}
+//
+//        for (int i = 1; i< 11; i++){      // 3 задание
+//            System.out.println("JAVA ");}
+
+        int year = 1980;   // 4 задание
+        while (year < 2021){
+            System.out.println ("Олимпиада " + year + " года");
+            year = year+4;}
+
+    }
+}
