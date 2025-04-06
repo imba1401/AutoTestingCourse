5ab1a2a8978171c8862a743e5409dc2c9ca927d1
imba1401 <imba_olga@mail.ru>
2025-04-06 12:16:34 +0300

    Day 16 Task1 дополнение по округлению до 3 знаков после запятой

--- src/day16/Task1.java
+++ src/day16/Task1.java
@@ -2,6 +2,7 @@ package src.day16;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Task1 {
@@ -11,12 +12,12 @@ public class Task1 {
         System.out.println(Task1.printSumDigits(file1));
     }
 
-    public static double printSumDigits(File file) {
+    public static String printSumDigits(File file) {
         try {
             Scanner scanner = new Scanner(file);
             String line = scanner.nextLine();
             String[] numbers = line.split(" ");
-            if (numbers.length != 10) {
+            if (numbers.length != 7) {
                 throw new IllegalArgumentException();
             }
             double sum = 0;
@@ -24,16 +25,17 @@ public class Task1 {
                 sum += Double.parseDouble(number);
             }
 
-            double average = sum/(double) numbers.length;
+            double average = sum / (double) numbers.length;
+            DecimalFormat myFormat = new DecimalFormat("#.###");
 
             scanner.close();
-            return average;
+            return average + " --> " + myFormat.format(average);
 
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         } catch (IllegalArgumentException e) {
             System.out.println("Некорректный входной файл");
         }
-        return 0;
+        return null;
     }
 }
